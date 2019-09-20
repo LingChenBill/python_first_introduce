2c0be376248d0e60e1f4dc57a07ad2c78042abf6
zhuyz <lingchen1316@163.com>
2019-09-20 23:34:13 +0800

    variable commit.

---
+++ 3.1-variable.py
@@ -0,0 +1,15 @@
+# Date:2019/9/20
+# Author:Lingchen
+# Mark:
+#   Python对大小写敏感。
+#   Python是无法打印不存在的对象的。
+#   语法问题，使用英文标点符号，大小写不要出错，空格不能少。
+#   要保持良好的命名习惯，应该尽量使用英文命名。可以发现英文教程也会阅读的很舒畅。
+
+answer = 46
+# print(a)
+print(answer)
+
+# 写入文件
+file = open('/Users/zhuyangze/Documents/fork/python_first_introduce/data/3.1-file.txt', 'w')
+file.write('life is short, you need Python')
